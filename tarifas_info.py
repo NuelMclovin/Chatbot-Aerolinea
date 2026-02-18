0609f7d194f08b2bb24a35543ab85fe335d9a0a1
Esau <em3518439@gmail.com>
2026-02-18 08:49:45 -0600

    Agregamos el diccionario para las tarifas

---
+++ tarifas_info.py
@@ -0,0 +1,21 @@
+tarifas_info = {
+            "1": {
+                "nombre": "Light",
+                "equipaje_mano": "1 maleta de mano (10 kg) + 1 artículo personal",
+                "equipaje_documentado": "No incluido (se compra por separado)",
+                "costo_primera": "$600 - $800 aproximado"
+            },
+            "2": {
+                "nombre": "Plus",
+                "equipaje_mano": "1 maleta de mano (10 kg) + 1 artículo personal",
+                "equipaje_documentado": "1 maleta documentada (25 kg) incluida",
+                "costo_adicional": "$800 - $1,200 por maleta adicional"
+            },
+            "3": {
+                "nombre": "Viva",
+                "equipaje_mano": "1 maleta de mano (10 kg) + 1 artículo personal",
+                "equipaje_documentado": "1 maleta documentada (25 kg) incluida",
+                "extras": "Asiento preferente y abordaje prioritario incluidos",
+                "costo_adicional": "$800 - $1,200 por maleta adicional"
+            }
+        }
